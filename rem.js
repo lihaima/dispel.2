a7f2df8317a3b91ffc665cbd44cd5e6d4084eb06
qingskd <2962948032@qq.com>
2022-10-21 11:54:03 +0800

    '2'

---
+++ rem.js
@@ -0,0 +1,9 @@
+;(function rem(){
+    let viewWidth = document.documentElement.clientWidth || window.innerWidth
+    if(viewWidth >= 1024) {
+        viewWidth = 1024
+    }else if(viewWidth <= 160){
+        viewWidth = 160
+    }
+    document.documentElement.style.fontSize = (viewWidth/375*1)+"px";
+})();
